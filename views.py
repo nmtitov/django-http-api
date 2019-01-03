4b1b76b1422723a25acd60dc5aab6a89d87e80dc
Nikita Titov <nmtitov@yandex.ru>
2019-01-03 21:46:50 +0200

    Delete http_api.error_views

--- views.py
+++ views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from .response import error
 
-# Create your views here.
+
+def handler400(request, exception):
+    return error("client-error", error_type="handler400", status=400)
+
+
+def handler403(request, exception):
+    return error("permission denied", error_type="handler403", status=403)
+
+
+def handler404(request, exception):
+    return error("not-found", error_type="handler404", status=404)
+
+
+def handler500(request):
+    return error("internal-server-error", error_type="handler500", status=500)
