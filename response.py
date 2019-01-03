3ef48bebdf09a7841b720b21992c918f0a188120
Nikita Titov <nmtitov@yandex.ru>
2019-01-03 17:44:56 +0200

    Refactor error

--- response.py
+++ response.py
@@ -12,20 +12,13 @@ def result(data, status=200):
 
 
 def error(name, error_type=None, message=None, status=500):
-    exc_type, value, traceback = exc_info()
-    exception = {
-        "exc_name": exc_type.__name__,
-        "exc_type": str(exc_type),
-        "value": str(value),
-        "traceback": format_exc().splitlines(),
-    }
     response = {
         "type": "error",
         "data": {
             "name": name,
-            "error_type": error_type,
+            "type": error_type,
             "message": message,
-            "exception": exception,
+            "exception": get_exception(),
         },
     }
     return JsonResponse(response, status=status, json_dumps_params={"indent": 2, "sort_keys": False})
@@ -33,3 +26,15 @@ def error(name, error_type=None, message=None, status=500):
 
 def method_not_allowed(message=None):
     return error("method-not-allowed", message=message, status=405)
+
+
+def get_exception():
+    type, value, traceback = exc_info()
+    if type and value and traceback:
+        return {
+            "name": "{module}.{name}".format(module=type.__module__, name=type.__name__),
+            "value": str(value),
+            "traceback": format_exc().splitlines(),
+        }
+    else:
+        return None
