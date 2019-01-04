9a0a343e9c8f29deadc37ca6f83153a1ff4b7bbf
Nikita Titov <nmtitov@yandex.ru>
2019-01-04 08:58:41 +0200

    Add custom error handlers comment

--- views.py
+++ views.py
@@ -1,5 +1,11 @@
 from .response import error
 
+# To enable handlers add the following lines to the project urls.py file
+# handler400 = 'http_api.views.handler400'
+# handler403 = 'http_api.views.handler403'
+# handler404 = 'http_api.views.handler404'
+# handler500 = 'http_api.views.handler500'
+
 
 def handler400(request, exception):
     return error("client-error", error_type="handler400", status=400)
