1cef3ec86cc02da50a2309b963c10da82018236c
Nikita Titov <nmtitov@yandex.ru>
2019-01-05 21:00:07 +0200

    Add authentication backend

---
+++ auth.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from .models import Session
+
+
+class SessionBackend(object):
+    def authenticate(self, request, token=None):
+        try:
+            session = Session.objects.get(token=token)
+            return session.user
+        except Session.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        user = get_user_model()
+        try:
+            return user.objects.get(pk=user_id)
+        except user.DoesNotExist:
+            return None
