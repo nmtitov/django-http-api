29f5d9c12b5a24cd68e3ef05c77416f1f230821e
Nikita Titov <nmtitov@yandex.ru>
2019-01-07 10:20:43 +0200

    Move authentication_required decorator to the top of the file

--- views.py
+++ views.py
@@ -13,6 +13,22 @@ def json_response(func):
     return decorator
 
 
+def authentication_required(func):
+    def decorator(request, *args, **kwargs):
+        # If a user did login to the admin panel before and has a cookie, let him in (works only in browser)
+        if request.user.is_authenticated:
+            return func(request, *args, **kwargs)
+        else:
+            token = request.META['HTTP_X_TOKEN']  # curl -i http://localhost:8000/api/ --header "X-Token: 000"
+            user = authenticate(request, token=token)
+            if user is not None:
+                login(request, user)
+                return func(request, *args, **kwargs)
+            else:
+                return error("Authentication required", error_type="authentication", status=403)
+    return decorator
+
+
 # To enable custom error handlers add the following lines to the project urls.py file
 # handler400 = 'http_api.views.handler400'
 # handler403 = 'http_api.views.handler403'
@@ -56,19 +72,3 @@ def handler500(request):
 @json_response
 def csrf_failure(request, reason):
     return error("csrf-failure", error_type="csrf_failure", exception=get_exception(), status=403)
-
-
-def authentication_required(func):
-    def decorator(request, *args, **kwargs):
-        # If a user did login to the admin panel before and has a cookie, let him in (works only in browser)
-        if request.user.is_authenticated:
-            return func(request, *args, **kwargs)
-        else:
-            token = request.META['HTTP_X_TOKEN']  # curl -i http://localhost:8000/api/ --header "X-Token: 000"
-            user = authenticate(request, token=token)
-            if user is not None:
-                login(request, user)
-                return func(request, *args, **kwargs)
-            else:
-                return error("Authentication required", error_type="authentication", status=403)
-    return decorator
