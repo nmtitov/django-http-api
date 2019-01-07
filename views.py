0a26cc176e3bce7ff78c6b8a53a1197bc6829818
Nikita Titov <nmtitov@yandex.ru>
2019-01-07 08:21:59 +0200

    Add auth comments

--- views.py
+++ views.py
@@ -54,13 +54,15 @@ def handler500(request):
 
 def authentication_required(func):
     def decorator(request, *args, **kwargs):
+        # If a user did login to the admin panel before and has a cookie, let him in (works only in browser)
         if request.user.is_authenticated:
             return func(request, *args, **kwargs)
-        token = request.META['HTTP_X_TOKEN']
-        user = authenticate(request, token=token)
-        if user is not None:
-            login(request, user)
-            return func(request, *args, **kwargs)
         else:
-            return error("Authentication required", error_type="authentication", status=403)
+            token = request.META['HTTP_X_TOKEN']  # curl -i http://localhost:8000/api/ --header "X-Token: 000"
+            user = authenticate(request, token=token)
+            if user is not None:
+                login(request, user)
+                return func(request, *args, **kwargs)
+            else:
+                return error("Authentication required", error_type="authentication", status=403)
     return decorator
