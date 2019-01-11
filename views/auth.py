6364a80bdee2cc56097a6d8ade32920169043b6c
Nikita Titov <nmtitov@yandex.ru>
2019-01-11 15:22:23 +0200

    Remove custom_auth.models dependency

--- views/auth.py
+++ views/auth.py
@@ -1,4 +1,4 @@
-from custom_auth.models import User
+from django.contrib.auth import get_user_model
 from django.core.exceptions import SuspiciousOperation
 from django.views.decorators.csrf import csrf_exempt
 from ..data import error, error_method_not_allowed, result
@@ -33,7 +33,8 @@ def signin(request):
         email = str(request.POST["email"])
         password = str(request.POST["password"])
         # Check password
-        user = User.objects.get(email=email)
+        user_model = get_user_model()
+        user = user_model.objects.get(email=email)
         if not user.check_password(password):
             return error("Invalid password", error_type="check_password", message="Password is invalid", status=400)
         # Create session object
