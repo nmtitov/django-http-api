6432df07f206f200df39a05bd06c4eabd4aae58e
Nikita Titov <nmtitov@yandex.ru>
2019-01-12 10:26:27 +0200

    Convert SessionBackend methods to static methods

--- http_api/authentication_backend.py
+++ http_api/authentication_backend.py
@@ -3,7 +3,8 @@ from .models import Session
 
 
 class SessionBackend:
-    def authenticate(self, request, token=None):
+    @staticmethod
+    def authenticate(request, token=None):
         try:
             session = Session.objects.get(token=token)
             session.update_last_seen()
@@ -12,7 +13,8 @@ class SessionBackend:
         except Session.DoesNotExist:
             return None
 
-    def get_user(self, user_id):
+    @staticmethod
+    def get_user(user_id):
         user = get_user_model()
         try:
             return user.objects.get(pk=user_id)
