6b61363b7c8ccbdce3406bdf62f7344b2365696c
Nikita Titov <nmtitov@yandex.ru>
2019-01-13 08:20:24 +0200

    Update error handler guide

--- http_api/views/error.py
+++ http_api/views/error.py
@@ -4,10 +4,10 @@ from ..utils.exceptions import get_exception
 
 
 # To enable custom error handlers add the following lines to the project urls.py file
-# handler400 = 'http_api.views.handler400'
-# handler403 = 'http_api.views.handler403'
-# handler404 = 'http_api.views.handler404'
-# handler500 = 'http_api.views.handler500'
+# handler400 = 'http_api.views.error.handler400'
+# handler403 = 'http_api.views.error.handler403'
+# handler404 = 'http_api.views.error.handler404'
+# handler500 = 'http_api.views.error.handler500'
 
 
 @json_response
