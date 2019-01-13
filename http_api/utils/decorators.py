7b287a6be69da0751503c927ce6f17d0423fba42
Nikita Titov <nmtitov@yandex.ru>
2019-01-13 21:56:33 +0200

    Use @wrap to keep function names in runtime

--- http_api/utils/decorators.py
+++ http_api/utils/decorators.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, JsonResponse
+from functools import wraps
 from ..utils.data_structures import error
 
 
 def json(func):
+    @wraps(func)
     def decorator(request, *args, **kwargs):
         x = func(request, *args, **kwargs)
         if x is None:
@@ -25,6 +27,7 @@ def _json_response(body, status):
 
 
 def auth(func):
+    @wraps(func)
     def decorator(request, *args, **kwargs):
         # If a user did login to the admin panel before and has a cookie, let him in (works only in browser)
         if request.user.is_authenticated:
