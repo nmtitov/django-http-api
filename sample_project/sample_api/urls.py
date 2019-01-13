0dee1b21f3b3f5ea147f5fb60442334625552623
Nikita Titov <nmtitov@yandex.ru>
2019-01-13 19:35:08 +0200

    Add more examples

--- sample_project/sample_api/urls.py
+++ sample_project/sample_api/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('clients', views.clients, name='clients'),
     path('secret', views.secret, name='secret'),
     path('user', views.user, name='user'),
+    path('empty', views.empty, name='empty'),
+    path('empty_status', views.empty_status, name='empty_status'),
+    path('greetings', views.greetings, name='greetings'),
+    path('united_states', views.united_states, name='united_states'),
 ]
--- sample_project/sample_api/views.py
+++ sample_project/sample_api/views.py
@@ -63,3 +63,58 @@ def secret(request):
         return result({"message": "This is my secret"})
     else:
         return error_method_not_allowed()
+
+
+# Empty response
+@csrf_exempt
+@json_response
+def empty(request):
+    return None
+
+
+# Empty response with a custom status code
+@csrf_exempt
+@json_response
+def empty_status(request):
+    return {
+        "status_code": 404,
+        "body": None,
+    }
+
+
+# List response
+@csrf_exempt
+@json_response
+def greetings(request):
+    return [{
+        "language": "English",
+        "greeting": "Hello",
+    }, {
+        "language": "French",
+        "greeting": "Bonjour",
+    }, {
+        "language": "Spanish",
+        "greeting": "Hola",
+    }, {
+        "language": "German",
+        "greeting": "Hallo",
+    }, {
+        "language": "Italian",
+        "greeting": "Ciao",
+    }, {
+        "language": "Portugese",
+        "greeting": "Ola",
+    }]
+
+
+# A plain dict response with a custom structure (keep in mind that "status_code" and "body" keys are reserved)
+@csrf_exempt
+@json_response
+def united_states(request):
+    return {
+        "country": "United States",
+        "capital": "Washington, D.C.",
+        "largest_city": "New York City",
+        "national_language": "English",
+        "area": "9833520",
+    }
