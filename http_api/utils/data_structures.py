d6b98591cee3e020d51b53d1f62d4625b83d4d86
Nikita Titov <nmtitov@yandex.ru>
2019-01-25 23:25:28 +0200

    Add display_name

--- http_api/utils/data_structures.py
+++ http_api/utils/data_structures.py
@@ -7,13 +7,14 @@ def result(data, status=200):
     }
 
 
-def error(name, error_type=None, display_message=None, exception_info=None, status=520):
+def error(name, error_type=None, display_name=None, display_message=None, exception_info=None, status=520):
     return {
         "status_code": status,
         "body": {
             "error": {
                 "name": name,
                 "type": error_type,
+                "display_name": display_name,
                 "display_message": display_message,
                 "exception_info": exception_info,
             }
