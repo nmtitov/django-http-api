b7c4246c11df92066fe66fbb715ebfe369f0e412
Nikita Titov <nmtitov@yandex.ru>
2019-01-25 15:45:24 +0200

    Rename get_exception -> get_exception_info

--- http_api/utils/exceptions.py
+++ http_api/utils/exceptions.py
@@ -2,7 +2,7 @@ from sys import exc_info
 from traceback import format_exc
 
 
-def get_exception():
+def get_exception_info():
     exc_type, value, traceback = exc_info()
     if exc_type and value and traceback:
         return {
--- http_api/views/error.py
+++ http_api/views/error.py
@@ -1,5 +1,5 @@
 from ..utils.data_structures import error
-from ..utils.exceptions import get_exception
+from ..utils.exceptions import get_exception_info
 from ..views.decorators.api import jsonify
 
 # To enable custom error handlers add the following lines to the project urls.py file
@@ -11,24 +11,24 @@ from ..views.decorators.api import jsonify
 
 @jsonify
 def handler400(request, exception):
-    return error("client-error", error_type="handler400", exception_info=get_exception(), status=400)
+    return error("client-error", error_type="handler400", exception_info=get_exception_info(), status=400)
 
 
 @jsonify
 def handler403(request, exception):
-    return error("permission-denied", error_type="handler403", exception_info=get_exception(), status=403)
+    return error("permission-denied", error_type="handler403", exception_info=get_exception_info(), status=403)
 
 
 @jsonify
 def handler404(request, exception):
-    return error("not-found", error_type="handler404", exception_info=get_exception(), status=404)
+    return error("not-found", error_type="handler404", exception_info=get_exception_info(), status=404)
 
 
 @jsonify
 def handler500(request):
-    return error("internal-server-error", error_type="handler500", exception_info=get_exception(), status=500)
+    return error("internal-server-error", error_type="handler500", exception_info=get_exception_info(), status=500)
 
 
 @jsonify
 def csrf_failure(request, reason=""):
-    return error("csrf-failure", error_type="csrf_failure", exception_info=get_exception(), status=403)
+    return error("csrf-failure", error_type="csrf_failure", exception_info=get_exception_info(), status=403)
