27fdca5190cdbedafcd262c5f68a1da58bb5bc30
Nikita Titov <nmtitov@yandex.ru>
2019-02-02 20:44:45 +0200

    Order sessions by created_at DESC

--- http_api/admin.py
+++ http_api/admin.py
@@ -10,6 +10,6 @@ username_field = f"user__{user_model.USERNAME_FIELD}"
 @admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
     list_filter = (username_field, )
-    ordering = ["created_at"]
+    ordering = ["-created_at"]
     readonly_fields = ("token", "created_at", "last_seen_at", )
     search_fields = [username_field]
