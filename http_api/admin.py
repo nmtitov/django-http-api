77a643b8bc4f07d345e2d16e71ebe42108a81d18
Nikita Titov <nmtitov@ya.ru>
2020-10-24 13:01:05 +0200

    Search by token

--- http_api/admin.py
+++ http_api/admin.py
@@ -3,15 +3,13 @@ from django.contrib.auth import get_user_model
 
 from .models import Session
 
-user_model = get_user_model()
-username_field = "user__{username_field}".format(
-    username_field=user_model.USERNAME_FIELD
-)
+username_field = f"user__{get_user_model().USERNAME_FIELD}"
 
 
 @admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
+    list_display = ("id", "created_at", "last_seen_at", )
     list_filter = (username_field, "created_at", )
     ordering = ["-created_at"]
-    readonly_fields = ("token", "created_at", "last_seen_at", )
-    search_fields = [username_field]
+    readonly_fields = ("id", "token", "created_at", "last_seen_at", )
+    search_fields = (username_field, "token", )
